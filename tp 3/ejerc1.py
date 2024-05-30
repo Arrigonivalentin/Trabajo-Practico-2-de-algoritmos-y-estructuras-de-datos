72069c0abff01e0e194a222b0896c02a52309822
Arrigoni Ricardo Valentin <141292029+Arrigonivalentin@users.noreply.github.com>
2024-05-30 00:07:45 -0300

    Delete tp 3/ejerc1.py

--- tp 3/ejerc1.py
+++
@@ -1,7 +0,0 @@
-num1=int(input("ingrese un numero: "))
-num2=int(input("ingrese el segundo numero: "))
-num3=int(input("ingrese el tercer numero: "))
-sum=num1+num2+num3
-print("el resultado de la suma es:",sum)
-producto=num1*num2*num3
-print("el resultado del producto es:",producto)
