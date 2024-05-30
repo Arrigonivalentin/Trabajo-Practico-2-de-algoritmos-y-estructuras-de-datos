d33700fbd3ad797328793c1edf2bd83a0a2b2e88
Arrigoni Ricardo Valentin <141292029+Arrigonivalentin@users.noreply.github.com>
2024-05-30 00:08:34 -0300

    Delete tp6 directory

--- tp6/ejerc1.py
+++
@@ -1,8 +0,0 @@
-def geometria (alto,ancho):
-    area=alto*ancho
-    return (area)
-
-alto=int(input("ingrese el alto: "))
-ancho=int(input("ingrese el ancho: "))
-
-print("el area del rectangulo es: ", geometria(alto, ancho))
--- tp6/ejerc6.py
+++
@@ -1,8 +0,0 @@
-def potencia (num,expon):
-    result=num**expon
-    return (result)
-
-num=float(input("ingrese un número: "))
-expon=float(input("ingrese el exponente: "))
-
-print("el resultado es: ",potencia(num,expon))
